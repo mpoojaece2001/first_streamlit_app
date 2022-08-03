87a346c2669a2b087208487f27f97c05ea2e0889
M Pooja <110520759+mpoojaece2001@users.noreply.github.com>
2022-08-03 12:14:28 +0530

    Update streamlit_app.py
    added menu as title
    added meal 
    added smoothie
    added egg

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title("My Parents New Healthy Dinner")
+streamlit.title("Breakfast Menu")
+streamlit.title("Omega 3 & Blueberry Oat meal")
+streamlit.title("Kale, Spinach & Rocket Smoothie")
+streamlit.title("Hard Boiled Free Range Egg")
